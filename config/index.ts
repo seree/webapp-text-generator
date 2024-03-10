e9fca5bc1db9dfa7a9772baed87ad685dfa3e6dc
Seree <seree.w@gmail.com>
2024-03-10 21:59:07 +0700

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
+  title: 'Cave AI App #1',
   description: 'App description',
   copyright: '',
   privacy_policy: '',
-  default_language: 'zh-Hans',
+  default_language: 'en-US',
 }
 
 export const API_PREFIX = '/api'
